3c9121037f1ab12312164388cc90707c4cb0a386
Edgar Lee <edgarhinshunlee@gmail.com>
2014-09-27 12:03:29 -0400

    Add basic carousel format and js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+function setCarouselSlide (slide) {
+  $('.carousel .slides section').eq(slide).addClass('current');
+  $('.carousel nav a').eq(slide).addClass('current');
+}
 
+$(function() {
+  var currentSlide = 0;
+  var numSlides = 3;
+
+  function changeSlide (change) {
+    $('.carousel .current').removeClass('current');
+    currentSlide = (currentSlide + change) % numSlides;
+    setCarouselSlide(currentSlide);
+  }
+
+  $('.carousel').on('click', '.next', function() {
+    changeSlide(1);
+  });
+
+  $('.carousel').on('click', '.previous', function() {
+    changeSlide(-1);
+  });
+});
